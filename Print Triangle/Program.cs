a3f01c0a00aa4c6c038e33b7e8a998db50ce64c9
Manikanta Varma <Manikanta Varma@ROCKSTARVARMA>
2023-12-20 22:28:33 +0530

    Add project files.

---
+++ Print Triangle/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace PrintingTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the height of the triangle:");
+
+            if (int.TryParse(Console.ReadLine(), out int height))
+            {
+                PrintTriangle(height);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input.Enter a valid integer.");
+            }
+        }
+
+        static void PrintTriangle(int height)
+        {
+            for (int i = 1; i <= height; i++)
+            {
+                for (int j = 0; j < height - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 0; k < 2 * i - 1; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
